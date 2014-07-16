d81379d91ed47b28d1e3ea5896e8ca620cc2f6ae
Teri Castle <tealynsea@hotmail.com>
2014-07-15 22:30:31 -0600

    working on battleship game

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*
 var studentList = [];
 
 //put a few students into array
@@ -59,7 +59,76 @@ $(document).on('ready', function() {
  
 renderList();
 
-});
+});*/
+
+
+var randomLoc = Math.floor(Math.random() * 5);
+var loc1 = randomLoc;
+var loc2 = loc1 + 1;
+var loc3 = loc2 + 1;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false) {
+
+	guess = prompt("Ready, aim, fire!  (enter a number from 0-6): ");
+
+		if (guess < 0 || guess >6) {
+			alert("Please enter a valid cell number");
+
+				
+
+		}	else {
+
+			guesses = guesses +1;
+		
+				
+
+				if (guess == loc1 || guess == loc2 || guess == loc3) {
+				
+				alert("hit");
+
+				hits = hits + 1;
+
+
+					if (hits == 3)  {
+
+					isSunk = true;
+
+					alert("You sank my battleship!");
+				}
+
+			}else {
+				alert('miss');
+
+			}
+		
+		}
+
+
+	}
+
+
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your accuracy was " + (3/guesses * 100 + "%");
+
+alert(stats);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  
